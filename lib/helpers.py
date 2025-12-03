b287317d6786f20da82413b23ad11166937b6860
George <gnm7208@gmail.com>
2025-12-04 00:50:22 +0300

    Implement find_employee_by_id function

--- lib/helpers.py
+++ lib/helpers.py
@@ -80,7 +80,9 @@ def find_employee_by_name():
 
 
 def find_employee_by_id():
-    pass
+    id_ = input("Enter the employee's id: ")
+    employee = Employee.find_by_id(id_)
+    print(employee) if employee else print(f'Employee {id_} not found')
 
 
 def create_employee():
